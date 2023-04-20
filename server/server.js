610d9862e657347ea3011b230cc60fbdbdd8e49e
ccislegend <84684162+ccislegend@users.noreply.github.com>
2023-04-19 17:48:41 -0800

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config({ path: "./server/.env" });
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const config = new Configuration({
+  organization: process.env.OPENAI_ORGANIZATION_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+console.log("Organization ID:", process.env.OPENAI_ORGANIZATION_ID);
+console.log("API Key:", process.env.OPENAI_API_KEY);
+
+const openai = new OpenAIApi(config);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  const { textInput } = req.body;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Convert the following text into an Air Force Achievement Medal Citation: ${textInput}`,
+    max_tokens: 256,
+    temperature: 0.7,
+    top_p: 1,
+  });
+
+  res.json({
+    response: response.data.choices[0].text,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/components/DecGenerator/DecGenerator.js
@@ -0,0 +1,65 @@
+// src/components/DecGenerator/DecGenerator.js
+import React, { useState } from "react";
+import "./DecGenerator.css";
+
+function DecGenerator() {
+  const [textInput, setTextInput] = useState("");
+  const [responseText, setResponseText] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:3001", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        textInput,
+      }),
+    });
+
+    const data = await response.json();
+    setResponseText(data.response);
+  };
+
+  const copyTextToClipboard = () => {
+    const textToCopy = document.getElementById("decResponse");
+    const textarea = document.createElement("textarea");
+    textarea.textContent = textToCopy.textContent;
+    textarea.style.position = "fixed"; // Prevent scrolling to bottom of the page on focus in iOS
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  };
+
+  return (
+    <div className="DecGenerator">
+      <form onSubmit={handleSubmit}>
+        <h1>Write My Dec</h1>
+        <label htmlFor="textInput">Enter bullets or text:</label>
+        <textarea
+          type="text"
+          id="textInput"
+          name="textInput"
+          style={{ overflow: "hidden" }}
+          value={textInput}
+          onChange={(event) => setTextInput(event.target.value)}
+        />
+        <button type="submit">Generate</button>
+      </form>
+      <div className="response-container">
+        <label htmlFor="response" id="responseLbl">
+          Generated Decoration Citation:
+        </label>
+        <p id="decResponse">{responseText}</p>
+        <button onClick={copyTextToClipboard} id="copyButton">
+          Copy Text
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DecGenerator;
